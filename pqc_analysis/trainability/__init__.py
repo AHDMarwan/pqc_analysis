ba6a4318de444bc53c162e9530232d7762b50208
MaitHad <marwan.aithaddou@edu.uca.ac.ma>
2026-08-11 23:58:22 +0100

    export gradient profiling APIs

--- pqc_analysis/trainability/__init__.py
+++ pqc_analysis/trainability/__init__.py
@@ -1,8 +1,11 @@
 from .barren_plateau import BarrenPlateauScanResult, barren_plateau_scan
 from .gradients import gradient_statistics
+from .profiles import GradientProfile, gradient_profile
 
 __all__ = [
     "gradient_statistics",
+    "GradientProfile",
+    "gradient_profile",
     "BarrenPlateauScanResult",
     "barren_plateau_scan",
 ]
