69bd6b3560ca2da55fe916d23b53f2f34683fd7c
MaitHad <marwan.aithaddou@edu.uca.ac.ma>
2026-08-11 23:33:16 +0100

    export barren plateau diagnostics

--- pqc_analysis/trainability/__init__.py
+++ pqc_analysis/trainability/__init__.py
@@ -1,3 +1,8 @@
+from .barren_plateau import BarrenPlateauScanResult, barren_plateau_scan
 from .gradients import gradient_statistics
 
-__all__ = ["gradient_statistics"]
+__all__ = [
+    "gradient_statistics",
+    "BarrenPlateauScanResult",
+    "barren_plateau_scan",
+]
