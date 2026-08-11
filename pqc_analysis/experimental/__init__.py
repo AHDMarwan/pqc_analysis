d921236de28d9befe0c104cff602afca9e032688
MaitHad <marwan.aithaddou@edu.uca.ac.ma>
2026-08-12 00:21:10 +0100

    export benchmark matrix APIs

--- pqc_analysis/experimental/__init__.py
+++ pqc_analysis/experimental/__init__.py
@@ -1,3 +1,17 @@
+from .benchmark_matrix import (
+    BenchmarkMatrixCase,
+    BenchmarkMatrixConfig,
+    build_benchmark_case,
+    build_benchmark_matrix,
+    matrix_to_study_specs,
+    parameter_count,
+)
+from .matrix_runner import (
+    DEFAULT_DIAGNOSTIC_METRICS,
+    DEFAULT_TOPOLOGY_METRICS,
+    MatrixExperimentResult,
+    run_benchmark_matrix_experiment,
+)
 from .study import TopologyStudyResult, TopologyStudySpec, run_topology_diagnostic_study
 from .topology_correlations import TopologyCorrelationResult, correlate_topology_with_diagnostics
 from .topology_metrics import PersistenceSummary, flatten_persistence_summaries, summarize_persistence_diagram
@@ -9,6 +23,16 @@ from .topology_state_space import (
 )
 
 __all__ = [
+    "BenchmarkMatrixConfig",
+    "BenchmarkMatrixCase",
+    "parameter_count",
+    "build_benchmark_case",
+    "build_benchmark_matrix",
+    "matrix_to_study_specs",
+    "MatrixExperimentResult",
+    "run_benchmark_matrix_experiment",
+    "DEFAULT_TOPOLOGY_METRICS",
+    "DEFAULT_DIAGNOSTIC_METRICS",
     "TopologyStudySpec",
     "TopologyStudyResult",
     "run_topology_diagnostic_study",
