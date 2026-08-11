476ef232630ffe66437f0b2fca652fbe822e4505
MaitHad <marwan.aithaddou@edu.uca.ac.ma>
2026-08-11 23:33:51 +0100

    align Python requirement with PennyLane

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="pqc_analysis",
     version="0.2.0",
     description="Trainability, geometry, and topology diagnostics for parameterized quantum circuits",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="AIT HADDOU Marwan",
     author_email="marwan.aithaddou@edu.uca.ac.ma",
@@ -22,10 +22,13 @@ setup(
         "scipy",
         "matplotlib",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.11",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
     ],
 )
