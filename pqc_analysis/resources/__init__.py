e8b0b6d6d8d6d502cbad9366ebfdbbf09f60ec9f
MaitHad <marwan.aithaddou@edu.uca.ac.ma>
2026-08-11 23:57:55 +0100

    add resource accounting package

---
+++ pqc_analysis/resources/__init__.py
@@ -0,0 +1,7 @@
+from .cost import ResourceEstimate, estimate_gradient_resources, estimate_training_resources
+
+__all__ = [
+    "ResourceEstimate",
+    "estimate_gradient_resources",
+    "estimate_training_resources",
+]
