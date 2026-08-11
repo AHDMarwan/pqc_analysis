ebdce55b5eaee2dd0c0653ef70365d15e0b92f30
MaitHad <marwan.aithaddou@edu.uca.ac.ma>
2026-08-12 00:17:45 +0100

    expose experimental topology research APIs

--- pqc_analysis/experimental/__init__.py
+++ pqc_analysis/experimental/__init__.py
@@ -1,6 +1,24 @@
+from .study import TopologyStudyResult, TopologyStudySpec, run_topology_diagnostic_study
 from .topology_correlations import TopologyCorrelationResult, correlate_topology_with_diagnostics
+from .topology_metrics import PersistenceSummary, flatten_persistence_summaries, summarize_persistence_diagram
+from .topology_state_space import (
+    TopologyStateSpaceResult,
+    analyze_topological_state_space,
+    pairwise_pure_state_bures,
+    pure_state_bures_distance,
+)
 
 __all__ = [
+    "TopologyStudySpec",
+    "TopologyStudyResult",
+    "run_topology_diagnostic_study",
     "TopologyCorrelationResult",
     "correlate_topology_with_diagnostics",
+    "PersistenceSummary",
+    "summarize_persistence_diagram",
+    "flatten_persistence_summaries",
+    "TopologyStateSpaceResult",
+    "analyze_topological_state_space",
+    "pure_state_bures_distance",
+    "pairwise_pure_state_bures",
 ]
