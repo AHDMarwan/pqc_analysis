8d262608e7fa5503c84125adb2af6f663a2ec51b
MaitHad <marwan.aithaddou@edu.uca.ac.ma>
2026-08-11 23:35:10 +0100

    export Qiskit adapter

--- pqc_analysis/adapters/__init__.py
+++ pqc_analysis/adapters/__init__.py
@@ -1,3 +1,9 @@
 from .pennylane import make_pennylane_gradient_fn, pennylane_barren_plateau_scan
+from .qiskit import make_qiskit_gradient_fn, qiskit_barren_plateau_scan
 
-__all__ = ["make_pennylane_gradient_fn", "pennylane_barren_plateau_scan"]
+__all__ = [
+    "make_pennylane_gradient_fn",
+    "pennylane_barren_plateau_scan",
+    "make_qiskit_gradient_fn",
+    "qiskit_barren_plateau_scan",
+]
